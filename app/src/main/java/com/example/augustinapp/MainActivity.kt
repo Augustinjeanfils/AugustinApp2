8026a649f2f8323a1dca3b75f79c5dfa69cb9063
Augustinjeanfils <166634946+Augustinjeanfils@users.noreply.github.com>
2025-08-30 13:46:55 -0400

    I add button to show or hide
    Add a button that will toggle hiding and showing the answer choices

--- app/src/main/java/com/example/augustinapp/MainActivity.kt
+++ app/src/main/java/com/example/augustinapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.augustinapp
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,7 +18,34 @@ class MainActivity : AppCompatActivity(){
 
         val textViewBadResponse1= findViewById<TextView>(R.id.textViewBadResponse1)
         val textViewBadResponse2= findViewById<TextView>(R.id.textViewBadResponse2)
+        val toggle_choices_visibility_true = findViewById<ImageView>(R.id.toggle_choices_visibility_true)
+        val toggle_choices_visibility_false = findViewById<ImageView>(R.id.toggle_choices_visibility_false)
 
+        // container response
+        val containResponse = findViewById<View>(R.id.containResponse)
+
+        // Hide Open eye Button
+        toggle_choices_visibility_true.visibility = View.INVISIBLE
+        // show Close eye Button
+        toggle_choices_visibility_false.visibility = View.VISIBLE
+
+
+        // handle Click on toggle_choices_visibility_false
+        toggle_choices_visibility_false.setOnClickListener {
+            toggle_choices_visibility_false.visibility = View.INVISIBLE
+            toggle_choices_visibility_true.visibility = View.VISIBLE
+            // Show response
+            containResponse.visibility = View.INVISIBLE
+        }
+
+        // handle Click on toggle_choices_visibility_tue
+        toggle_choices_visibility_true.setOnClickListener {
+            toggle_choices_visibility_false.visibility = View.VISIBLE
+            toggle_choices_visibility_true.visibility = View.INVISIBLE
+            // Show response
+            containResponse.visibility = View.VISIBLE
+        }
+        
         textViewBadResponse1.setOnClickListener {
             textViewBadResponse1.setBackgroundColor(getResources().getColor(R.color.red, null))
         }
@@ -26,8 +54,13 @@ class MainActivity : AppCompatActivity(){
             textViewBadResponse2.setBackgroundColor(getResources().getColor(R.color.red, null))
         }
 
+        // Handle Bonne reponse
         texviewBonneReponseButton.setOnClickListener {
             texviewBonneReponseButton.setBackgroundColor(getResources().getColor(R.color.green, null))
+            // reset bad response back to blue
+            textViewBadResponse1.setBackgroundColor(getResources().getColor(R.color.blue, null))
+            textViewBadResponse2.setBackgroundColor(getResources().getColor(R.color.blue, null))
+
         }
     }
 }
